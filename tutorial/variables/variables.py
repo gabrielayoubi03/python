81faf19f361633a7fd1d1b7f1972c1310ae4b1de
Gabriel Ayoubi <gabrielayoubi03@gmail.com>
2024-02-25 23:44:55 -0500

    feat: added initial tutorial progress

---
+++ tutorial/variables/variables.py
@@ -0,0 +1,20 @@
+# Variable - container for a value. Behaves as the value that it contains.
+
+firstName = "Gabriel"
+lastName = "Ayoubi"
+fullName = firstName + " " + lastName
+# print(type(name))
+print("Hello " + fullName)
+
+age = 21
+age += 1
+print("Your age is: " + str(age))
+# print(type(age))
+
+height = 190.5
+print("Your height is: " + str(height) + "cm")
+# print(type(height))
+
+human = True
+print("Are you a human: " + str(human))
+# print(type(human))
